82169470cf5c137eac44d77ac744d06d76ee191d
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-05 00:13:03 +0600

    Update inheritance_example.rb

--- src/inheritance_example.rb
+++ src/inheritance_example.rb
@@ -1,1 +1,16 @@
+# Inheritance in Ruby
+class Animal
+  def speak
+    puts "Animal speaks"
+  end
+end
+
+class Dog < Animal
+  def speak
+    puts "Dog barks"
+  end
+end
+
+dog = Dog.new
+dog.speak
 
